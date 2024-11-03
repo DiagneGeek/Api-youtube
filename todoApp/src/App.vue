aad0b12cb8f693c0e7255a4f5f35d0be1defea25
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2024-11-03 11:14:46 +0000

    yes

---
+++ todoApp/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const todos = ref([])
+  const newTodo = ref('')
+
+  const addTodo = () => {
+     todos.value.push({
+        title: newTodo.value,
+        completed: false,
+        id: todos.value.length
+     })
+     newTodo = ''
+  }
+  
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input type="text" placeholder="add a todo" 
+           v-model="newTodo"/>
+    <button type="submit">add</button>
+  </form>
+
+  <div v-if="todos.length === 0"> you haven't a todo</div>
+  <div v-else>
+     <ul>
+       <li v-for="todo in todos" :key="id" v-show="todo.completed == false"> 
+        <button @click="todo.completed = true ">delete</button>
+        {{ todo.title }}
+
+       </li>
+     </ul>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
